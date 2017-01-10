12bfe63940ca6f18f0917a3f8bc6c5996ac0749d
Ali GOREN <goren.ali@yandex.com>
2017-01-10 21:42:48 +0300

    Update ColorLog.js

--- ColorLog.js
+++ ColorLog.js
@@ -2,33 +2,33 @@
 	@created: Ali GÖREN - 2017-01-10
 */
 class Log {
-	constructor() {
-		 this.LogDate = '[' + new Date().toLocaleString() + '] ';
+    constructor() {
+	 this.LogDate = '[' + new Date().toLocaleString() + '] ';
 
-		 this.InfoLabel = '[INFO] ';
-		 this.InfoColor = 'background: #3b5998; color: #fff'
+	 this.InfoLabel = '[INFO] ';
+	 this.InfoColor = 'background: #3b5998; color: #fff'
 
-		 this.DebugLabel = '[DEBUG] ';
-		 this.DebugColor = 'background: #008000; color: #fff'
+	 this.DebugLabel = '[DEBUG] ';
+	 this.DebugColor = 'background: #008000; color: #fff'
 
-		 this.ErrorLabel = '[ERROR] ';
-		 this.ErrorColor = 'background: #dc0000; color: #fff'
+	 this.ErrorLabel = '[ERROR] ';
+	 this.ErrorColor = 'background: #dc0000; color: #fff'
 
-		 this.CriticalLabel = '[CRITICAL] ';
-		 this.CriticalColor = 'background: #f00; color: #fff'
+	 this.CriticalLabel = '[CRITICAL] ';
+	 this.CriticalColor = 'background: #f00; color: #fff'
 
-		 this.WarningLabel = '[WARNING] ';
-		 this.WarningColor = 'background: #ffa500; color: #fff'
+	 this.WarningLabel = '[WARNING] ';
+	 this.WarningColor = 'background: #ffa500; color: #fff'
 
-		 this.TraceLabel = '[TRACE] ';
-		 this.TraceColor = 'background: #76272D; color: #fff'
+	 this.TraceLabel = '[TRACE] ';
+	 this.TraceColor = 'background: #76272D; color: #fff'
     }
     
     Info(msg) {
     	console.log('%c' + this.LogDate + this.InfoLabel + msg + ' ', this.InfoColor);
     }
 
-		Debug(msg) {
+    Debug(msg) {
     	console.log('%c' + this.LogDate + this.DebugLabel + msg + ' ', this.DebugColor);
     }
 	
